4171732bd484019b0879b76824e4b2699f8a41c9
rOluochKe <rasholuoch@gmail.com>
2019-12-17 00:31:16 +0300

    users validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validate :username, presence :true, length {min :6, max :12}
+  valid_email_regex = /^[a-zA-Z0-9.!#$%&'*+\/=​?^_`{|}~-]+@​[a-zA-Z0-9](?:​[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:​\.[a-zA-Z0-9](?:​[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+  validate :email, presence :true length {min :6, max :50}, format {with :valid_email_regex}
+  uniqueness :true
 end

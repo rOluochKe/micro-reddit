b2611c5d36960d6e7928776d73abafa1e2a47df8
ampaire <femayapr@gmail.com>
2019-12-16 15:55:21 -0600

    Add the validations for the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validate :title, presence :true, length {min :6, max :100}
+  validate :description, presence :true, length {min :6, max :400}
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   valid_email_regex = /^[a-zA-Z0-9.!#$%&'*+\/=​?^_`{|}~-]+@​[a-zA-Z0-9](?:​[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:​\.[a-zA-Z0-9](?:​[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
   validate :email, presence :true length {min :6, max :50}, format {with :valid_email_regex}
   uniqueness :true
+
+  has_many :posts
 end
